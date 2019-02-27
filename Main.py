031549076a07341219303864cb64ec4e16fecec7
GranDurismo <rxaviermontero@gmail.com>
2019-02-27 02:20:45 -0300

    Convert extracted string to date

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from pandas.io.json import json_normalize
 import pandas as pd
 import requests
 import re
+from datetime import datetime
 
 with open('API key.txt','r') as key:
     key = key.read()
@@ -13,4 +14,5 @@ r = requests.get('https://api.darksky.net/forecast/%s/%s,%s,%s' %(key, latitude,
                  '?lang=es&units=si&exclude=currently,minutely,hourly,alerts,flags')
 raw = r.json()
 df = pd.concat([json_normalize(raw), json_normalize(raw['daily']['data'])], axis=1)
-df['daily.data'] = re.search('time\': (.+?),', str(df.iloc[0, 0])).group(1)
+df['daily.data'] = datetime.fromtimestamp(int(re.search('time\': (.+?),',
+  str(df.iloc[0, 0])).group(1))).strftime('%d-%m-%Y')
