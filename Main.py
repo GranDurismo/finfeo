31b3ace11986dc349023ec4cb596498a0eab4929
GranDurismo <rxaviermontero@gmail.com>
2019-02-28 01:58:32 -0300

    Simplify df building

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
-from pandas.io.json import json_normalize
 import pandas as pd
 import requests
-import re
-from datetime import datetime
 import json
 
 with open('API key.txt','r') as key:
@@ -14,6 +11,19 @@ date = '2017-01-01T00:00:00'
 r = requests.get('https://api.darksky.net/forecast/%s/%s,%s,%s' %(key, latitude, longitude, date) + 
                  '?lang=es&units=si&exclude=currently,minutely,hourly,alerts,flags').text
 raw = json.loads(r)
-df = pd.concat([json_normalize(raw), json_normalize(raw['daily']['data'])], axis=1)
-df['daily.data'] = datetime.fromtimestamp(int(re.search('time\': (.+?),',
-  str(df.iloc[0, 0])).group(1))).strftime('%d-%m-%Y')
+
+vars = ['time','summary','temperatureMin','temperatureMax','apparentTemperatureMin',
+        'apparentTemperatureMax','precipIntensity','precipIntensityMax',
+        'precipProbability','cloudCover','humidity','windSpeed','windGust']
+
+df = []
+for i in vars:
+    df_aux = pd.DataFrame({'Date': [date],
+                 'Latitude': [raw['latitude']],
+                 'Longitude': [raw['longitude']],
+                 i: [raw['daily']['data'][0][i]],
+                }).set_index('Date')
+    df.append(df_aux)
+df = pd.concat(df, axis=1)
+df['time'] = pd.to_datetime(df['time'], unit='s')
+
