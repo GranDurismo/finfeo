a335942738cc6b863153a6545d6cc19bad424d65
GranDurismo <rxaviermontero@gmail.com>
2019-03-07 00:42:59 -0300

    Create range of dates and separate in chunks

--- Main.py
+++ Main.py
@@ -1,14 +1,22 @@
 import pandas as pd
+import numpy as np
 import requests
 import json
 from pandas.io.json import json_normalize
+from datetime import date, timedelta
 
 with open('API key.txt','r') as key:
     key = key.read()
     
 latitude = -34.915
 longitude = -56.165
-date = ['2017-01-01T00:00:00', '2016-01-01T00:00:00']
+
+date_end = date(2018, 12, 31)
+num_days = 950
+num_chunks = 5
+
+date_list = [date_end - timedelta(days=x) for x in range(0, num_days*num_chunks)]
+date_chunks = np.array_split(date_list, num_chunks)
 
 raw = []
 for i in date:
