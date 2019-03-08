57e16728e07a9d6854bf2994cbea86ed09ca8e35
GranDurismo <rxaviermontero@gmail.com>
2019-03-08 00:10:39 -0300

    Define function for request loops that creates dataframes

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 import pandas as pd
-import numpy as np
 import requests
 import json
+import pickle
 from pandas.io.json import json_normalize
 from datetime import date, timedelta
 
 latitude = -34.915
 longitude = -56.165
-date_end = date(2018, 12, 31)
-num_days = 3
-num_chunks = 2
+first_date = date(2018, 12, 31)
+num_days = 6
 
 time_suffix = 'T00:00:00'
 darksky_url = 'https://api.darksky.net/forecast'
@@ -17,18 +16,20 @@ darksky_suffix = '?lang=es&units=si&exclude=currently,minutely,hourly,alerts,fla
 
 with open('API_key.txt','r') as f:
     key = f.read()
-    
-date_list = [date_end - timedelta(days=x) for x in range(0, num_days*num_chunks)]
-date_list_str = [str(x) + time_suffix for x in date_list]
-date_chunks = np.array_split(date_list_str, num_chunks)
 
-raw = []
-for i in range(0, num_chunks):
-    for dates in date_chunks[i]:
+def request_loop(date_start, raw, df):
+    date_list = [date_start - timedelta(days=x) for x in range(0, num_days)]
+    date_list_str = [str(x) + time_suffix for x in date_list]
+    for dates in date_list_str:
         get = requests.get(f'{darksky_url}/{key}/{latitude},{longitude},{dates}{darksky_suffix}').text
         reqs_aux = json.loads(get)
         raw.append(reqs_aux)
-
+        df_date = json_normalize(raw[date_list_str.index(dates)]['daily']['data'])
+        df.append(df_date)
+    df = pd.concat(df, axis=0, ignore_index=True, sort=False)
+    pickle.dump(raw, open('raw.p', 'wb'))
+    pickle.dump(df, open('df.p', 'wb'))
+    
 df_raw = []
 for i in date:
     df_date = json_normalize(raw[date.index(i)]['daily']['data'])
